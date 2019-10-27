0f6910c30c3e033be871d1ca17635145553a9329
MarekGn <marek.gna@gmail.com>
2019-10-28 00:08:16 +0100

    [Networks] refactor network for stock prediction

--- Networks/RNN.py
+++ Networks/RNN.py
@@ -1,40 +1,39 @@
-#Load Packages
 import numpy as np
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense
-from tensorflow.python.keras.layers import LSTM
+from tensorflow.python.keras.layers import Dense, LSTM, Activation, Dropout
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.python.keras.layers import Activation
-from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.utils.generic_utils import get_custom_objects
+from tensorflow.python.keras import optimizers, backend as K
 from tensorflow.python.keras.callbacks import EarlyStopping
 
 class RNN:
     def __init__(self, lookback):
         self.trainX = None
         self.trainY = None
-        self.predX = None
-        self.accuracy = None
-        self.model = None
+        self.testX = None
+        self.testY = None
         self.lookback = lookback
+        self.model = None
         self.layer_sizes = ''
-        self.Yscaler = MinMaxScaler(feature_range=(0, 1))
-        self.Xscaler = MinMaxScaler(feature_range=(0, 1))
-
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
         get_custom_objects().update({'bent': Bent(bent)})
 
-    def predict(self, x):
-        self.create_dataset(x=x, training=False)
-        prediction = self.model.predict(self.predX, batch_size=1)
-        prediction = self.Yscaler.inverse_transform(prediction)
-        return prediction
+    # def predict(self, x):
+    #     self.create_dataset(x=x, training=False)
+    #     prediction = self.model.predict(self.predX, batch_size=1)
+    #     prediction = self.Yscaler.inverse_transform(prediction)
+    #     return prediction
 
     def create_model(self):
         self.model = Sequential()
-        self.model.add(LSTM(units=32, input_shape=(1, self.lookback), return_sequences=True, activation='bent'))
-        self.model.add(LSTM(units=32, return_sequences=False, activation='bent'))
+        self.model.add(LSTM(units=128, input_shape=(self.lookback, self.trainX.shape[2]), return_sequences=True, activation='bent'))
+        self.model.add(Dropout(0.2))
+        self.model.add(LSTM(units=128, return_sequences=False, activation='bent'))
+        self.model.add(Dropout(0.2))
+        self.model.add(Dense(units=64, activation='bent'))
+        self.model.add(Dropout(0.2))
         self.model.add(Dense(units=32, activation='bent'))
-        self.model.add(Dense(units=16, activation='bent'))
+        self.model.add(Dropout(0.2))
         self.model.add(Dense(units=1, activation='bent'))
 
         self.layer_sizes = ''
@@ -42,51 +41,44 @@ class RNN:
             self.layer_sizes += str(l.output_shape)+" "
 
     def configure(self):
-        self.model.compile(optimizer='adam', loss="mean_squared_error")
+        optimizer = optimizers.RMSprop(lr=1e-8, rho=0.9)
+        self.model.compile(optimizer=optimizer, loss="mean_squared_error")
 
-    def train(self, batch_size=32, epochs=1000):
-        es = EarlyStopping(monitor='loss', patience=15000, min_delta=1e-10)
-        self.model.fit(self.trainX, self.trainY, epochs=epochs, batch_size=batch_size, verbose=1, shuffle=True,
-                       callbacks=[es])
+    def train(self, batchSize=32, epochs=100):
+        es = EarlyStopping(monitor='loss', patience=15000, min_delta=1e-6)
+        self.model.fit(self.trainX, self.trainY, epochs=epochs, batch_size=batchSize, verbose=2, shuffle=True, callbacks=[es])
 
-    def create_training_dataset_with_squence(self, x, dataset):
-        dataX, dataY = [], []
-        for i in range(len(dataset)-(self.lookback-1)):
-            a = x[i:(i+self.lookback), 0]
-            dataX.append(a)
-            dataY.append(dataset[i + (self.lookback-1), 0])
-        dataX = np.array(dataX)
-        dataY = np.array(dataY)
+    # def check_mse_accuracy(self, x, y):
+    #     x = x.reshape((-1, 1))
+    #     y = y.reshape((-1, 1))
+    #     x = self.Xscaler.transform(x)
+    #     x, _ = self.create_training_dataset_with_squence(x, y)
+    #     predictedY = self.Yscaler.inverse_transform(self.model.predict(x))
+    #     self.accuracy = np.mean(np.square(y - predictedY))
+    #     print("RNN accuracy is {}".format(self.accuracy))
+    #     return self.accuracy
 
-        # LSTM network must have input with 3 dimensions
-        dataX = np.reshape(dataX, (dataX.shape[0], 1, dataX.shape[1]))
-        return dataX, dataY
+    def create_training_dataset_with_squence(self, trainData, outputIdx):
+        # outputIdx is the index of out output column
+        dim_0 = trainData.shape[0] - self.lookback
+        dim_1 = trainData.shape[1]
+        x = np.zeros((dim_0, self.lookback, dim_1))
+        y = np.zeros((dim_0,))
 
-    def create_dataset(self, x, y=None, training=False):
-        x = x.reshape((-1, 1))
-        if y is None:
-            y = x.copy()
-        y = y.reshape((-1, 1))
-        if training is True:
-            # Network Training
-            y = self.Yscaler.fit_transform(y)
-            x = self.Xscaler.fit_transform(x)
-            self.trainX, self.trainY = self.create_training_dataset_with_squence(x, y)
-        else:
-            # Network Predicting
-            y = self.Yscaler.transform(y)
-            x = self.Xscaler.transform(x)
-            self.predX, _ = self.create_training_dataset_with_squence(x, y)
+        for i in range(dim_0):
+            x[i] = trainData[i:self.lookback + i]
+            y[i] = trainData[self.lookback + i, outputIdx]
+        print("shape of Input and Output data ", np.shape(x), np.shape(y))
+        self.trainX = x
+        self.trainY = y
 
-    def check_accuracy(self, x, y):
-        x = x.reshape((-1, 1))
-        y = y.reshape((-1, 1))
-        x = self.Xscaler.transform(x)
-        x, _ = self.create_training_dataset_with_squence(x, y)
-        predictedY = self.Yscaler.inverse_transform(self.model.predict(x))
-        self.accuracy = np.mean(np.square(y - predictedY))
-        print("RNN accuracy is {}".format(self.accuracy))
-        return self.accuracy
+    def scale_and_fit_data(self, data):
+        data = self.scaler.fit_transform(data)
+        return data
+
+    def scale_data(self, data):
+        data = self.scaler.transform(data)
+        return data
 
 
 class Bent(Activation):
